02b69b55003ecb660c7c1504c5201c1d34684dfe
jaehongjun <gatsby0391@naver.com>
2020-05-25 17:32:56 +0900

    upstream remote

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./store/reducers";
 import sagas from "./store/sagas";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import createSagaMiddleware from "redux-saga";
 
 const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
-
+const middleware = [sagaMiddleware];
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(...middleware))
+    : composeWithDevTools(applyMiddleware(...middleware));
+const store = createStore(reducers, enhancer);
 sagaMiddleware.run(sagas);
 
 render(
